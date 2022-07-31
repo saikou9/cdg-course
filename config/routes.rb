2dfaf7538925fa2baca8bdfaf98d56845dd14122
Sobolev Max <twenty2001@mail.ru>
2022-07-31 22:13:02 +0300

    Homepage links was updated for logined users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :accounts
 
-  get "/dashboard" => "accounts#index"
+  get "/dashboard", to: "accounts#index"
+  delete "/logout", to: "devise/sessions#destroy"
 
   resources :posts, only: [:new, :create, :show]
 
   root to: "static_pages#homepage"
+
 end
